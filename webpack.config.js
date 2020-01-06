65e119d4d132f065132ab2b28e7fa12391e26135
Max Scher <maxobaxo@gmail.com>
2020-01-05 21:07:06 -0800

    Configure Repo for JS w/ webpack

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/themes/embodied-kiddos/js/index.js', './src/themes/embodied-kiddos/stylesheets/main.scss'],
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'src', 'themes', 'embodied-kiddos', 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader?sourceMap',
+          'postcss-loader',
+          'resolve-url-loader',
+          'sass-loader?sourceMap'
+        ]
+      },
+      {
+        test: /\.(svg|png|jpg|gif|ttf|otf|eot|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!(dom7|ssr-window|swiper)\/).*/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ],
+  resolve: {
+    alias: {
+
+    }
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true,
+        cache: true,
+        parallel: true
+      }),
+      new OptimizeCSSAssetsPlugin()
+    ]
+  }
+};
