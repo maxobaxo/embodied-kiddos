6e331c2d35b66bc820dc85fa6f53dd12a0abab8f
Max Scher <maxobaxo@gmail.com>
2020-01-13 21:00:23 -0800

    Implement more seamless, smooth designs

--- src/themes/embodied-kiddos/header.php
+++ src/themes/embodied-kiddos/header.php
@@ -31,9 +31,8 @@
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<?php bloginfo( 'name' ); ?>
-					</a>
+					<?php bloginfo( 'name' ); ?>
+
 				</h1>
 				<?php
 			else :
