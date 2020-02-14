6b13d5cd865e4f00899e9a7fc85058b058ca8788
Max Scher <maxobaxo@gmail.com>
2020-02-13 22:32:05 -0800

    Enqueue bootstrap styles/scripts

--- src/themes/embodied-kiddos/functions.php
+++ src/themes/embodied-kiddos/functions.php
@@ -120,8 +120,15 @@ add_action( 'widgets_init', 'embodied_kiddos_widgets_init' );
  * Enqueue scripts and styles.
  */
 function embodied_kiddos_scripts() {
+	// Bootstrap Styles & Scripts
+	wp_enqueue_style( 'bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' );
+	wp_enqueue_script( 'bootstrap-js-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array( 'jquery' ), '3.4.1', true );
+	wp_enqueue_script( 'bootstrap-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true );
+	wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true );
+
 	wp_enqueue_style( 'embodied-kiddos-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'embodied-kiddos-webpack-style', get_template_directory_uri() . '/dist/main.css', array(), '20200107, true');
+
 	wp_enqueue_script( 'embodied-kiddos-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'embodied-kiddos-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
